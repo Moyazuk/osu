be59fdd7f0c69f8a0c2d7a78740c19de8f78c694
Kris <moyazuk@gmail.com>
2024-11-09 16:31:35 -0500

    Revert "more stuff, no clue what I'm doing"
    This reverts commit 0591589c35582fd63a9f0c749cf52d5f3d275acb.

--- osu.Game.Rulesets.Osu/Difficulty/Skills/ContinuousStrainSkill.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/ContinuousStrainSkill.cs
@@ -8,7 +8,6 @@ using osu.Game.Rulesets.Mods;
 using System.Linq;
 using osu.Framework.Utils;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
-using AlsaSharp;
 
 namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 {
@@ -18,7 +17,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         /// <summary>
         /// The final multiplier to be applied to <see cref="DifficultyValue"/> after all other calculations.
         /// </summary>
-        protected virtual double DifficultyMultiplier => 1.00;
+        protected virtual double DifficultyMultiplier => 1.50;
 
         protected virtual double SectionLength => 400;
         protected virtual double DecayWeight => 0.9;
@@ -40,76 +39,53 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         }
 
         public override double DifficultyValue()
+{
+    double result = 0.0;
+    double currentWeight = 1;
+    double frequency = 0;
+    var sortedStrains = strains.OrderByDescending(x => (x.Strain, x.StrainCountChange)).ToList();
+
+    double strainDecayRate = Math.Log(StrainDecayBase) / 1000;
+    double sumDecayRate = Math.Log(DecayWeight) / SectionLength;
+
+    double strainIntegral = 0.0;
+    for (int i = 0; i < sortedStrains.Count - 1; ++i)
+    {
+        var current = sortedStrains[i];
+        var next = sortedStrains[i + 1];
+
+        double averageStrain = (current.Strain + next.Strain) / 2;
+        double deltaTime = next.StrainCountChange; 
+        strainIntegral += averageStrain * deltaTime;
+    }
+
+
+    if (sortedStrains.Count > 0)
+    {
+        var last = sortedStrains.Last();
+        strainIntegral += last.Strain * last.StrainCountChange;
+    }
+
+    for (int i = 0; i < sortedStrains.Count - 1; ++i)
+    {
+        var current = sortedStrains[i];
+        var next = sortedStrains[i + 1];
+        frequency += current.StrainCountChange;
+
+        if (frequency > 0 && current.Strain > 0)
         {
-            double result = 0.0;
-            double currentWeight = 1;
-            double frequency = 0;
-            var sortedStrains = strains.OrderByDescending(x => (x.Strain, x.StrainCountChange)).ToList();
+            double time = Math.Log(next.Strain / current.Strain) * (frequency / strainDecayRate);
 
-            double strainDecayRate = Math.Log(StrainDecayBase) / 1000;
-            double sumDecayRate = Math.Log(DecayWeight) / SectionLength;
-            double strainIntegral = StrainIntegral();
+            double nextWeight = currentWeight * Math.Exp(sumDecayRate * time);
 
-            for (int i = 0; i < sortedStrains.Count - 1; ++i)
-            {
-                var current = sortedStrains[i];
-                var next = sortedStrains[i + 1];
-                frequency += current.StrainCountChange;
-
-                if (frequency > 0 && current.Strain > 0)
-                {
-                    double time = Math.Log(next.Strain / current.Strain) * (frequency / strainDecayRate);
-
-                    double nextWeight = currentWeight * Math.Exp(sumDecayRate * time);
-
-                    double combinedDecay = SectionLength * (sumDecayRate + (strainDecayRate / frequency));
-                    result += (next.Strain * nextWeight - current.Strain * currentWeight) / combinedDecay;
-                    currentWeight = nextWeight;
-                }
-            }
-
-            return result * DifficultyMultiplier;
+            double combinedDecay = SectionLength * (sumDecayRate + (strainDecayRate / frequency));
+            result += (next.Strain * nextWeight - current.Strain * currentWeight) / combinedDecay;
+            currentWeight = nextWeight;
         }
-
-        public double StrainIntegral()
-        {
-            var sortedStrains = strains.OrderByDescending(x => (x.Strain, x.StrainCountChange)).ToList();
-
-            double strainDecayRate = Math.Log(StrainDecayBase) / 1000;
-            double sumDecayRate = Math.Log(DecayWeight) / SectionLength;
-
-            double strainIntegral = 0.0;
-
-            if (sortedStrains.Count == 0)
-                return 0.0; // Return 0 if no strains
-
-
-            double maxStrain = sortedStrains.Max(x => x.Strain);
-
-            if (maxStrain == 0)
-                return 0.0;
-
-            double normalizationFactor = 10 / maxStrain;
-
-            for (int i = 0; i < sortedStrains.Count - 1; ++i)
-            {
-                var current = sortedStrains[i];
-                var next = sortedStrains[i + 1];
-
-                double normalizedCurrentStrain = current.Strain * normalizationFactor;
-                double normalizedNextStrain = next.Strain * normalizationFactor;
-
-                double averageStrain = (normalizedCurrentStrain + normalizedNextStrain) / 2;
-                double deltaTime = next.StrainCountChange;
-                strainIntegral += averageStrain * deltaTime;
-            }
-
-            var last = sortedStrains.Last();
-            double normalizedLastStrain = last.Strain * normalizationFactor;
-            strainIntegral += normalizedLastStrain * last.StrainCountChange;
-            Console.WriteLine($"StrainIntegral: {strainIntegral}");
-            return strainIntegral;
-        }
+    }
+    double lengthBonus = 1 + Math.Log(strainIntegral, 10) * 0.05;
+    return result * lengthBonus * DifficultyMultiplier;
+}
 
 
         protected abstract double StrainValueAt(DifficultyHitObject hitObject);
