e23aec64e17bf3ad822702f4922f598d25411ca8
Kris <moyazuk@gmail.com>
2025-03-15 22:28:42 -0400

    more stuff idk

--- osu.Game.Rulesets.Osu/Difficulty/Aggregation/OsuTimeSkill.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Aggregation/OsuTimeSkill.cs
@@ -19,7 +19,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Aggregation
         }
 
         // Assume players spend 12 minutes retrying a map before they FC
-        private const double time_threshold = 12;
+        private static double time_threshold => 30;
 
         // The width of each dimension of the bins. Since the array of bins is 2 dimensional, the number of bins is equal to these values multiplied together.
         private const int difficulty_bin_count = 8;
--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/SnapAimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/SnapAimEvaluator.cs
@@ -16,9 +16,9 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
         public static double EvaluateDifficultyOf(DifficultyHitObject current)
         {
             // Base snap difficulty is velocity.
-            double difficulty = EvaluateDistanceBonus(current) * 225;
+            double difficulty = EvaluateDistanceBonus(current) * 240;
             //difficulty += EvaluateAgilityBonus(current) * 65;
-            difficulty += EvaluateAngleBonus(current) * 95;
+            difficulty += EvaluateAngleBonus(current) * 175;
             difficulty += EvaluateVelocityChangeBonus(current) * 20;
 
             return difficulty;
@@ -53,12 +53,12 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             double currentAngle = osuCurrObj.Angle!.Value * 180 / Math.PI;
 
             // We reward high bpm more for wider angles, but only when both current and previous distance are over 0.5 radii.
-            double baseBpm = 240.0 / (1 + 0.45 * Smootherstep(currentAngle, 0, 120) * currDistanceMultiplier * prevDistanceMultiplier);
+            double baseBpm = 240.0 / (1 + 0.25 * Smootherstep(currentAngle, 0, 120) * currDistanceMultiplier * prevDistanceMultiplier);
 
             // Agility bonus of 1 at base BPM.
-            double agilityBonus = Math.Max(0, Math.Pow(MillisecondsToBPM(Math.Max(currTime, prevTime), 2) / baseBpm, 4) - 1);
+            double agilityBonus = Math.Max(0, Math.Pow(MillisecondsToBPM(Math.Max(currTime, prevTime), 2) / baseBpm, 6) - 1);
 
-            return agilityBonus * 28;
+            return agilityBonus * 12;
         }
 
         public static double EvaluateAngleBonus(DifficultyHitObject current)
