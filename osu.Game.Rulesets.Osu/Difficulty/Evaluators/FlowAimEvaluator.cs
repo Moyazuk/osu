a0fe3f00dca7bca98a71a5485b67f265b4cc8368
Kris <moyazuk@gmail.com>
2025-03-23 12:04:46 -0400

    -stamina (again)

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/FlowAimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/FlowAimEvaluator.cs
@@ -13,7 +13,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
     public static class FlowAimEvaluator
     {
         // The reason why this exist in evaluator instead of FlowAim skill - it's because it's very important to keep flowaim in the same scaling as snapaim on evaluator level
-        private static double flowMultiplier => 485.2;
+        private static double flowMultiplier => 485.21;
 
         public static double EvaluateDifficultyOf(DifficultyHitObject current)
         {
