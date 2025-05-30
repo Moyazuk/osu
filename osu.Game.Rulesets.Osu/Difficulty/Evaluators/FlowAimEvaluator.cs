1d825d37c1962d554e2f9abc0e131ee6beda5f64
Givikap120 <givikap120@gmail.com>
2025-05-30 17:09:33 +0300

    Update FlowAimEvaluator.cs

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/FlowAimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/FlowAimEvaluator.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Linq;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Difficulty.Utils;
 using osu.Game.Rulesets.Osu.Difficulty.Preprocessing;
