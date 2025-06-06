83fa97cd47a5c7c46a44eb0146fa0c3839fc29e4
Kris <moyazuk@gmail.com>
2025-06-06 13:25:47 -0400

    good enough

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
@@ -19,9 +19,9 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
     public class Speed : OsuStrainSkill
     {
         private double totalMultiplier => 1.0;
-        private double burstMultiplier => 3.10;
-        private double streamMultiplier => 0.095;
-        private double staminaMultiplier => 0.04;
+        private double burstMultiplier => 2.95;
+        private double streamMultiplier => 0.065;
+        private double staminaMultiplier => 0.07;
         private double meanFactor => 1.25;
 
         private double currentBurstStrain;
