3c5fb0eb2f14968293cd99f225f5a8ed15ac52f9
Givikap120 <givikap120@gmail.com>
2025-06-23 18:47:31 +0300

    adjust performance base multiplier

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -26,7 +26,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         private double currentStrain;
 
-        private double skillMultiplier => 23.25;
+        private double skillMultiplier => 24.48;
         private double strainDecayBase => 0.15;
 
         private readonly List<double> sliderStrains = new List<double>();
