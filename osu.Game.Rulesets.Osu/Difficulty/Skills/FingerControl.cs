6bea15cfaa09a7af5072511331aa32dc6db91a68
Kris <moyazuk@gmail.com>
2025-09-01 17:03:20 -0400

    yeah sure

--- osu.Game.Rulesets.Osu/Difficulty/Skills/FingerControl.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/FingerControl.cs
@@ -19,7 +19,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
     /// </summary>
     public class FingerControl : Skill
     {
-        private double skillMultiplier => 8;
+        private double skillMultiplier => 2;
 
         private readonly List<double> noteDifficulties = new List<double>();
 
