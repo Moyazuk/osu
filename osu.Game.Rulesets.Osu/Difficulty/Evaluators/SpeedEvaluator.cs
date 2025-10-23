760ef2e293a81675bb3f365d1f944d63be538e90
Xexxar <xexxar.xexxar@gmail.com>
2025-10-23 14:15:40 -0500

    clean up residual code

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/SpeedEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/SpeedEvaluator.cs
@@ -35,10 +35,9 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             var osuCurrObj = (OsuDifficultyHitObject)current;
             var osuPrevObj = osuCurrObj.TapIndex is > 0 ? (OsuDifficultyHitObject)osuCurrObj.PreviousTap(0) : null;
 
-            if (!osuCurrObj.IsTapObject || osuPrevObj is null || !osuPrevObj.IsTapObject)
+            if (!osuCurrObj.IsTapObject)
                 return 0;
 
-//            double strainTime = (osuCurrObj.TapStrainTime + osuPrevObj.TapStrainTime) / 2.0;
             double strainTime = osuCurrObj.TapStrainTime;
             double doubletapness = osuCurrObj.IsTapObject ? 1.0 - osuCurrObj.GetDoubletapness((OsuDifficultyHitObject?)osuCurrObj.NextTap(0)) : 1;
 
@@ -54,7 +53,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
                 speedBonus += 0.75 * Math.Pow((DifficultyCalculationUtils.BPMToMilliseconds(min_speed_bonus) - strainTime) / speed_balancing_factor, 2);
 
             // Base difficulty with all bonuses
-//            double difficulty = (1.0 + speedBonus) * 1000 / Math.Min(75, strainTime);
             double difficulty = 0;
 
             if (strainTime > 83.333)
