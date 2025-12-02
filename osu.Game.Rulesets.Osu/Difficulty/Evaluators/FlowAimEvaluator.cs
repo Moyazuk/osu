14fa0bd39b82c79de1d8d8a19c7da5caf5bc7a23
kwotaq <PJXORISJ@gmail.com>
2025-12-02 18:48:23 +0200

    buff flow aim a bit

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/FlowAimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/FlowAimEvaluator.cs
@@ -75,7 +75,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
 
             flowDifficulty += angleBonus;
 
-            flowDifficulty *= 1.0;
+            flowDifficulty *= 1.05;
 
             if (osuLast0Obj.BaseObject is Slider && withSliderTravelDistance)
             {
