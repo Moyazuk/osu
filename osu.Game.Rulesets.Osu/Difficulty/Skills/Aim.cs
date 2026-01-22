3590a2d9ea660edb24c01e1e155966b343057d1f
Natelytle <92956514+Natelytle@users.noreply.github.com>
2026-01-22 14:05:21 -0500

    Move strain's influence in the difficulty of a note based on deltaTime to the evaluator level (#36417)
    This PR moves the influence of d/t^2 from the skill (through strain)
    directly into the evaluator level as a bonus applied at the end. This
    makes it more clear what d/t^2 is, and the fact that it applies to *all*
    bonuses in the evaluator. As well, the peak strain value of a note is
    now equal to the evaluator value. This comes with a couple benefits:
    
    1. The BPM weight is now subject to balance, and can be flattened easily
    for a more "d/t" like system (previously this required hacky solutions)
    2. StrainDecayBase becomes a much more useful variable now that it does
    not affect the difficulty of the note itself. When adjusting this in
    live, your star rating would double upon changing from 0.15 in aim to
    0.3, and now it is intuitive what it does (makes strain take longer to
    accumulate). This means that future balancing efforts can use evaluators
    to dynamically adjust strainDecayBase (potentially letting large spikes
    provide more strain for the same difficulty if they're wide angle, for
    example).
    3. In the object inspector, you get the actual maximum difficulty value
    of a note as seen in the ObjectDifficulties list. This makes it easier
    to tell what notes are deemed as harder by the system. For context,
    previously, a note of difficulty 1 at 200bpm would cap out at 6 as a
    result of strain, and a note of the same difficulty but at 300bpm would
    cap out at 8.
    
    The actual implementation is really really simple. I'm willing to move
    this to flashlight if wanted (I don't think it's necessary), or even
    abstract this away so that (1 - decay) doesn't look like a balancing
    constant.
    
    Side note: this is equivalent to live, except for notes with a deltaTime
    of less than 25ms (since I am using AdjustedDeltaTime in the aim
    evaluator for the bonus to avoid divisions by zero).

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -6,9 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Mods;
-using osu.Game.Rulesets.Osu.Difficulty.Aggregation;
 using osu.Game.Rulesets.Osu.Difficulty.Evaluators;
-using osu.Game.Rulesets.Difficulty.Utils;
+using osu.Game.Rulesets.Osu.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Osu.Difficulty.Utils;
 using osu.Game.Rulesets.Osu.Objects;
 
@@ -17,7 +16,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
     /// <summary>
     /// Represents the skill required to correctly aim at every object in the map with a uniform CircleSize and normalized distances.
     /// </summary>
-    public class Aim : OsuFcTimeSkill
+    public class Aim : OsuStrainSkill
     {
         public readonly bool IncludeSliders;
 
@@ -29,25 +28,21 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         private double currentStrain;
 
-        private double skillMultiplier => 130;
+        private double skillMultiplier => 26;
         private double strainDecayBase => 0.15;
 
         private readonly List<double> sliderStrains = new List<double>();
 
-        protected override double HitProbability(double skill, double difficulty)
-        {
-            if (difficulty <= 0) return 1;
-            if (skill <= 0) return 0;
-
-            return DifficultyCalculationUtils.Erf(skill / (Math.Sqrt(2) * difficulty));
-        }
-
         private double strainDecay(double ms) => Math.Pow(strainDecayBase, ms / 1000);
+
+        protected override double CalculateInitialStrain(double time, DifficultyHitObject current) => currentStrain * strainDecay(time - current.Previous(0).StartTime);
 
         protected override double StrainValueAt(DifficultyHitObject current)
         {
-            currentStrain *= strainDecay(current.DeltaTime);
-            currentStrain += AimEvaluator.EvaluateDifficultyOf(current, IncludeSliders) * skillMultiplier;
+            double decay = strainDecay(((OsuDifficultyHitObject)current).AdjustedDeltaTime);
+
+            currentStrain *= decay;
+            currentStrain += AimEvaluator.EvaluateDifficultyOf(current, IncludeSliders) * (1 - decay) * skillMultiplier;
 
             if (current.BaseObject is Slider)
                 sliderStrains.Add(currentStrain);
